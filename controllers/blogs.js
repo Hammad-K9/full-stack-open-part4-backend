bb7d78404f85ecc406828675000e3d88565f3766
Hammad-K9 <37565310+Hammad-K9@users.noreply.github.com>
2024-02-26 17:33:04 -0500

    Completed exercises 4.11-4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,13 +6,27 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const { body } = request;
 
-  const blog = new Blog({ ...body });
+  const blog = new Blog({ ...body, likes: body.likes || 0 });
 
   const savedBlog = await blog.save();
   response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = { ...request.body };
+
+  const updatedContact = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true
+  });
+  response.json(updatedContact);
 });
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -15,14 +15,14 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs);
 });
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
   await api
     .get('/api/bloglist')
     .expect(200)
     .expect('Content-Type', /application\/json/);
 });
 
-test('there are two notes', async () => {
+test('there are two blogs', async () => {
   const response = await api.get('/api/bloglist');
 
   assert.strictEqual(response.body.length, 2);
@@ -36,7 +36,7 @@ test('all blogs have id property rather than _id property', async () => {
   });
 });
 
-test('a valid note can be added ', async () => {
+test('a valid blog can be added', async () => {
   const newBlog = {
     title: 'new title',
     author: 'new author',
@@ -57,6 +57,74 @@ test('a valid note can be added ', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length + 1);
 
   assert.deepStrictEqual(mostRecentBlog, newBlog);
+});
+
+test('likes defaults to 0 when not specified', async () => {
+  const newBlog = {
+    title: 'new title',
+    author: 'new author',
+    url: 'www.newwebsite.com'
+  };
+
+  await api.post('/api/bloglist').send(newBlog);
+
+  const response = await api.get('/api/bloglist');
+
+  const { likes } = response.body[response.body.length - 1];
+
+  assert.strictEqual(likes, 0);
+});
+
+test('fails with statuscode 400 when title or url is missing', async () => {
+  let newBlog = {
+    author: 'new author'
+  };
+
+  await api.post('/api/bloglist').send(newBlog).expect(400);
+
+  newBlog = {
+    title: 'new title',
+    author: 'new author'
+  };
+
+  await api.post('/api/bloglist').send(newBlog).expect(400);
+
+  newBlog = {
+    author: 'new author',
+    url: 'www.newwebsite.com'
+  };
+
+  await api.post('/api/bloglist').send(newBlog).expect(400);
+});
+
+test('blog is deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api.delete(`/api/bloglist/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  blogsAtEnd.forEach((blog) => {
+    assert.notDeepStrictEqual(blog, blogToDelete);
+  });
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+});
+
+test('blog is updated', async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToUpdate = blogsAtStart[0];
+
+  await api
+    .put(`/api/bloglist/${blogToUpdate.id}`)
+    .send({ ...blogToUpdate, likes: blogToUpdate.likes + 1 })
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  const updatedBlog = blogsAtEnd[0];
+  assert.strictEqual(updatedBlog.likes, blogToUpdate.likes + 1);
 });
 
 after(async () => {
